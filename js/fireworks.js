0468fff26249fecab7f22b8c17ff53e49ce86e47
zhao xinnnn <zhaoxinnnn@zhaodeMacBook-Pro.local>
2022-04-08 11:14:55 +0800

    feat：重建项目

--- js/fireworks.js
+++
@@ -1,5 +0,0 @@
-<% if (theme.fireworks.enable) { %>
-  <canvas class="fireworks" style="position: fixed; left: 0; top: 0; z-index: 1; pointer-events: none;" ></canvas>
-  <script type="text/javascript" src="//cdn.bootcss.com/animejs/2.2.0/anime.min.js"></script>
-  <script type="text/javascript" src="/js/fireworks.js"></script>
-  <% } %>
